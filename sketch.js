cf55b010345bf68e913e7a5684f12b41304792d9
Gravestarrrr <onlygravestar@gmail.com>
2026-04-12 14:43:40 -0400

    collage1

--- sketch.js
+++ sketch.js
@@ -1,7 +1,30 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(400,400);
 }
 
 function draw() {
-  background(220);
+  background('red');
+  
+  let flowerX = 200;
+  let flowerY = 200;
+  let petalSize = 100;
+  let petalDistance = petalSize / 2;
+
+  fill(255, 128, 0);
+
+  // upper-left petal
+  circle(flowerX - petalDistance, flowerY - petalDistance, petalSize);
+
+  // upper-right petal
+  circle(flowerX + petalDistance, flowerY - petalDistance, petalSize);
+
+  // lower-left petal
+  circle(flowerX - petalDistance, flowerY + petalDistance, petalSize);
+
+  // lower-right petal
+  circle(flowerX + petalDistance, flowerY + petalDistance, petalSize );
+
+  // center petal
+  fill(255, 0, 0);
+  circle(flowerX, flowerY, petalSize);
 }
