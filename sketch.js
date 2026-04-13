e538defb5b11286fcc91f0fa544f0545a8440209
Gravestarrrr <onlygravestar@gmail.com>
2026-04-12 22:11:03 -0400

    adding stuff

--- sketch.js
+++ sketch.js
@@ -1,33 +1,50 @@
+// Define the global variables.
+// The symmetry variable will define how many reflective sections the canvas
+// is split into.
+let symmetry = 6;
+
+// The angle button will calculate the angle at which each section is rotated.
+let angle = 360 / symmetry;
+
 function setup() {
-  createCanvas(400,8000);
+  describe(
+    `Dark rgb(0,0,0) canvas that reflects the lines drawn within it in ${symmetry} sections.`
+  );
+  createCanvas(windowWidth, 800);
+  angleMode(DEGREES);
+  background(255, 63, 0);
 }
 
 function draw() {
-  background('red');
+  // Move the 0,0 coordinates of the canvas to the center, instead of in
+  // the top left corner.
+  translate(width / 2, height / 2);
 
-  rect(500, 500, 400, 200);
-  fill('black');
-  
-  let flowerX = 200;
-  let flowerY = 200;
-  let petalSize = 100;
-  let petalDistance = petalSize / 2;
+  // If the cursor is within the limits of the canvas...
+  if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
+    // Translate the current position and the previous position of the
+    // cursor to the new coordinates set with the translate() function above.
+    let lineStartX = mouseX - width / 2;
+    let lineStartY = mouseY - height / 2;
+    let lineEndX = pmouseX - width / 2;
+    let lineEndY = pmouseY - height / 2;
 
-  fill(255, 128, 0);
+    // And, if the mouse is pressed while in the canvas...
+    if (mouseIsPressed === true) {
+      // For every reflective section the canvas is split into, draw the cursor's
+      // coordinates while pressed...
+      for (let i = 0; i < symmetry; i++) {
+        rotate(angle);
+        stroke(255);
+        strokeWeight(3);
+        line(lineStartX, lineStartY, lineEndX, lineEndY);
 
-  // upper-left petal
-  circle(flowerX - petalDistance, flowerY - petalDistance, petalSize);
-
-  // upper-right petal
-  circle(flowerX + petalDistance, flowerY - petalDistance, petalSize);
-
-  // lower-left petal
-  circle(flowerX - petalDistance, flowerY + petalDistance, petalSize);
-
-  // lower-right petal
-  circle(flowerX + petalDistance, flowerY + petalDistance, petalSize );
-
-  // center petal
-  fill(255, 0, 0);
-  circle(flowerX, flowerY, petalSize);
+        // ... and reflect the line within the symmetry sections as well.
+        push();
+        scale(1, -1);
+        line(lineStartX, lineStartY, lineEndX, lineEndY);
+        pop();
+      }
+    }
+  }
 }
